6c516415f5040b8fa0327e91e0987cb5dc2c9c63
Thet Naing Tun <thetnaingtun@bindez.com>
2021-09-09 11:43:59 +0630

    fix unnecessary re-rendering issue in app-left-controls and app-right-controls

--- src/components/app-left-controls/index.tsx
+++ src/components/app-left-controls/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useMemo } from "react";
 import {
   Row as AntdRow,
   Col as AntdCol,
@@ -63,129 +63,134 @@ export default function AppLeftControls() {
     gradientCoverage,
   ]);
 
-  const props: UploadProps = {
-    name: "file",
-    action: "https://www.mocky.io/v2/5cc8019d300000980a055e76",
-    headers: {
-      authorization: "authorization-text",
-    },
-    onChange(info) {
-      if (info.file.status !== "uploading") {
-        setLoading(true);
-      }
-      if (info.file.status === "done") {
-        setFileName(info.file.name);
-        getBase64(info.file.originFileObj, (imgUrl) => {
-          setLoading(false);
-          setPreviewVisible(true);
-          setPreviewImage(imgUrl);
-          if (info.fileList.length > 1) info.fileList.shift();
-        });
-      }
-      if (info.file.status === "removed") {
-        setPreviewVisible(false);
-        setPreviewImage("qijin-xu.png");
-      }
-    },
-  };
-
-  return (
-    <section>
-      <AntdRow>
-        <AntdCol span={6}>
-          <label htmlFor="gradientOverlay">Gradient Type</label>
-        </AntdCol>
-        <AntdCol span={18}>
-          <AntdRadio.Group
-            value={gradientOverlay}
-            onChange={(e) => setGradientOverlay(e.target.value)}
-            defaultValue="circular"
-          >
-            <AntdRadio.Button value="circular">Circular</AntdRadio.Button>
-            <AntdRadio.Button value="up">Up</AntdRadio.Button>
-            <AntdRadio.Button value="down">Down</AntdRadio.Button>
-            <AntdRadio.Button value="diagonaldown">↘</AntdRadio.Button>
-            <AntdRadio.Button value="diagonalup">↗</AntdRadio.Button>
-            <AntdRadio.Button value="none">None</AntdRadio.Button>
-          </AntdRadio.Group>
-        </AntdCol>
-      </AntdRow>
-      <AntdRow>
-        <AntdCol span={6}>
-          <label htmlFor="gradientCoverage">Gradient Coverage</label>
-        </AntdCol>
-        <AntdCol span={13}>
-          <AntdSlider
-            onChange={(value) => setGradientCoverage(value)}
-            min={0}
-            max={100}
-            value={gradientCoverage}
-          />
-        </AntdCol>
-        <AntdCol span={5}>
-          <AntdInputNumber
-            min={0}
-            max={100}
-            onChange={(value) => setGradientCoverage(value)}
-            value={gradientCoverage}
-            id="gradientCoverage"
-          />
-        </AntdCol>
-      </AntdRow>
-      <AntdRow>
-        <AntdCol span={6}>
-          <label htmlFor="buttonRadius">Button Radius</label>
-        </AntdCol>
-        <AntdCol span={13}>
-          <AntdSlider
-            onChange={(value) => setButtonRadius(value)}
-            min={1}
-            max={40}
-            value={buttonRadius}
-          />
-        </AntdCol>
-        <AntdCol span={5}>
-          <AntdInputNumber
-            min={1}
-            max={40}
-            onChange={(value) => setButtonRadius(value)}
-            value={buttonRadius}
-            id="buttonRadius"
-          />
-        </AntdCol>
-      </AntdRow>
-      <AntdRow>
-        <AntdCol span={6}>
-          <label htmlFor="titleSpacing">Title Spacing</label>
-        </AntdCol>
-        <AntdCol span={13}>
-          <AntdSlider
-            onChange={(value) => setTitleSpacing(value)}
-            min={1}
-            max={300}
-            value={titleSpacing}
-          />
-        </AntdCol>
-        <AntdCol span={5}>
-          <AntdInputNumber
-            min={1}
-            max={300}
-            onChange={(value) => setTitleSpacing(value)}
-            value={titleSpacing}
-            id="titleSpacing"
-          />
-        </AntdCol>
-      </AntdRow>
-      <AntdRow>
-        <AntdCol span={6}>
-          <label htmlFor="upload">Upload a new image</label>
-        </AntdCol>
-        <AntdCol span={18}>
-          <AntdUpload {...props}>
-            <AntdButton icon={<UploadOutlined />}>Click to upload</AntdButton>
-          </AntdUpload>
-        </AntdCol>
-      </AntdRow>
-    </section>
+  const props: UploadProps = useMemo(
+    () => ({
+      name: "file",
+      action: "https://www.mocky.io/v2/5cc8019d300000980a055e76",
+      headers: {
+        authorization: "authorization-text",
+      },
+      onChange(info) {
+        if (info.file.status !== "uploading") {
+          setLoading(true);
+        }
+        if (info.file.status === "done") {
+          setFileName(info.file.name);
+          getBase64(info.file.originFileObj, (imgUrl) => {
+            setLoading(false);
+            setPreviewVisible(true);
+            setPreviewImage(imgUrl);
+            if (info.fileList.length > 1) info.fileList.shift();
+          });
+        }
+        if (info.file.status === "removed") {
+          setPreviewVisible(false);
+          setPreviewImage("qijin-xu.png");
+        }
+      },
+    }),
+    []
   );
+
+  return useMemo(() => {
+    return (
+      <section>
+        <AntdRow>
+          <AntdCol span={6}>
+            <label htmlFor="gradientOverlay">Gradient Type</label>
+          </AntdCol>
+          <AntdCol span={18}>
+            <AntdRadio.Group
+              value={gradientOverlay}
+              onChange={(e) => setGradientOverlay(e.target.value)}
+              defaultValue="circular"
+            >
+              <AntdRadio.Button value="circular">Circular</AntdRadio.Button>
+              <AntdRadio.Button value="up">Up</AntdRadio.Button>
+              <AntdRadio.Button value="down">Down</AntdRadio.Button>
+              <AntdRadio.Button value="diagonaldown">↘</AntdRadio.Button>
+              <AntdRadio.Button value="diagonalup">↗</AntdRadio.Button>
+              <AntdRadio.Button value="none">None</AntdRadio.Button>
+            </AntdRadio.Group>
+          </AntdCol>
+        </AntdRow>
+        <AntdRow>
+          <AntdCol span={6}>
+            <label htmlFor="gradientCoverage">Gradient Coverage</label>
+          </AntdCol>
+          <AntdCol span={13}>
+            <AntdSlider
+              onChange={(value) => setGradientCoverage(value)}
+              min={0}
+              max={100}
+              value={gradientCoverage}
+            />
+          </AntdCol>
+          <AntdCol span={5}>
+            <AntdInputNumber
+              min={0}
+              max={100}
+              onChange={(value) => setGradientCoverage(value)}
+              value={gradientCoverage}
+              id="gradientCoverage"
+            />
+          </AntdCol>
+        </AntdRow>
+        <AntdRow>
+          <AntdCol span={6}>
+            <label htmlFor="buttonRadius">Button Radius</label>
+          </AntdCol>
+          <AntdCol span={13}>
+            <AntdSlider
+              onChange={(value) => setButtonRadius(value)}
+              min={1}
+              max={40}
+              value={buttonRadius}
+            />
+          </AntdCol>
+          <AntdCol span={5}>
+            <AntdInputNumber
+              min={1}
+              max={40}
+              onChange={(value) => setButtonRadius(value)}
+              value={buttonRadius}
+              id="buttonRadius"
+            />
+          </AntdCol>
+        </AntdRow>
+        <AntdRow>
+          <AntdCol span={6}>
+            <label htmlFor="titleSpacing">Title Spacing</label>
+          </AntdCol>
+          <AntdCol span={13}>
+            <AntdSlider
+              onChange={(value) => setTitleSpacing(value)}
+              min={1}
+              max={300}
+              value={titleSpacing}
+            />
+          </AntdCol>
+          <AntdCol span={5}>
+            <AntdInputNumber
+              min={1}
+              max={300}
+              onChange={(value) => setTitleSpacing(value)}
+              value={titleSpacing}
+              id="titleSpacing"
+            />
+          </AntdCol>
+        </AntdRow>
+        <AntdRow>
+          <AntdCol span={6}>
+            <label htmlFor="upload">Upload a new image</label>
+          </AntdCol>
+          <AntdCol span={18}>
+            <AntdUpload {...props}>
+              <AntdButton icon={<UploadOutlined />}>Click to upload</AntdButton>
+            </AntdUpload>
+          </AntdCol>
+        </AntdRow>
+      </section>
+    );
+  }, [buttonRadius, titleSpacing, gradientOverlay, gradientCoverage, props]);
 }
--- src/components/app-right-controls/index.tsx
+++ src/components/app-right-controls/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { Row as AntdRow, Col as AntdCol, Switch as AntdSwitch } from "antd";
 
 import { useOptionsContext } from "../../context/option-context";
@@ -28,58 +28,60 @@ export default function AppRightControls() {
     });
   }, [button, buttonColor, buttonHoverColor, gradientColor, dispatch]);
 
-  return (
-    <section>
-      <AntdRow>
-        <AntdCol span={12}>
-          <label htmlFor="button">Button</label>
-        </AntdCol>
-        <AntdCol span={12}>
-          <AntdSwitch
-            style={{ background: "#63b6b8" }}
-            onChange={() => setButton((val) => !val)}
-          />
-        </AntdCol>
-      </AntdRow>
-      <AntdRow>
-        <AntdCol span={12}>
-          <label htmlFor="buttonColor">Button Color</label>
-        </AntdCol>
-        <AntdCol span={12}>
-          <input
-            id="buttonColor"
-            type="color"
-            onChange={(e) => setButtonColor(e.target.value)}
-            value={buttonColor}
-          />
-        </AntdCol>
-      </AntdRow>
-      <AntdRow>
-        <AntdCol span={12}>
-          <label htmlFor="buttonHoverColor">Button Hover Color</label>
-        </AntdCol>
-        <AntdCol span={12}>
-          <input
-            id="buttonHoverColor"
-            type="color"
-            onChange={(e) => setButtonHoverColor(e.target.value)}
-            value={buttonHoverColor}
-          />
-        </AntdCol>
-      </AntdRow>
-      <AntdRow>
-        <AntdCol span={12}>
-          <label htmlFor="gradientColor">Gradient Color</label>
-        </AntdCol>
-        <AntdCol span={12}>
-          <input
-            id="gradientColor"
-            type="color"
-            onChange={(e) => setGradientColor(e.target.value)}
-            value={gradientColor}
-          />
-        </AntdCol>
-      </AntdRow>
-    </section>
-  );
+  return useMemo(() => {
+    return (
+      <section>
+        <AntdRow>
+          <AntdCol span={12}>
+            <label htmlFor="button">Button</label>
+          </AntdCol>
+          <AntdCol span={12}>
+            <AntdSwitch
+              style={{ background: "#63b6b8" }}
+              onChange={() => setButton((val) => !val)}
+            />
+          </AntdCol>
+        </AntdRow>
+        <AntdRow>
+          <AntdCol span={12}>
+            <label htmlFor="buttonColor">Button Color</label>
+          </AntdCol>
+          <AntdCol span={12}>
+            <input
+              id="buttonColor"
+              type="color"
+              onChange={(e) => setButtonColor(e.target.value)}
+              value={buttonColor}
+            />
+          </AntdCol>
+        </AntdRow>
+        <AntdRow>
+          <AntdCol span={12}>
+            <label htmlFor="buttonHoverColor">Button Hover Color</label>
+          </AntdCol>
+          <AntdCol span={12}>
+            <input
+              id="buttonHoverColor"
+              type="color"
+              onChange={(e) => setButtonHoverColor(e.target.value)}
+              value={buttonHoverColor}
+            />
+          </AntdCol>
+        </AntdRow>
+        <AntdRow>
+          <AntdCol span={12}>
+            <label htmlFor="gradientColor">Gradient Color</label>
+          </AntdCol>
+          <AntdCol span={12}>
+            <input
+              id="gradientColor"
+              type="color"
+              onChange={(e) => setGradientColor(e.target.value)}
+              value={gradientColor}
+            />
+          </AntdCol>
+        </AntdRow>
+      </section>
+    );
+  }, [buttonColor, buttonHoverColor, gradientColor]);
 }
